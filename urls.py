51acc276576582f0fbe2111b1c5315cc42362d70
YashJain98 <41756100+YashJain98@users.noreply.github.com>
2018-07-26 23:32:55 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,8 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('parsifal.account_settings.views',
-    url(r'^$', 'settings', name='settings'),
-    url(r'^profile/$', 'profile', name='profile'),
-    url(r'^emails/$', 'emails', name='emails'),
-    url(r'^picture/$', 'picture', name='picture'),
-    url(r'^password/$', 'password', name='password'),
-
-    url(r'^connections/$', 'connections', name='connections'),
-    
-    url(r'^mendeley_connection/$', 'mendeley_connection', name='mendeley_connection'),
-    url(r'^connect_mendeley/$', 'connect_mendeley', name='connect_mendeley'),
-    url(r'^disconnect_mendeley/$', 'disconnect_mendeley', name='disconnect_mendeley'),
-
-    url(r'^dropbox_connection/$', 'dropbox_connection', name='dropbox_connection'),
-    url(r'^connect_dropbox/$', 'connect_dropbox', name='connect_dropbox'),
-    url(r'^disconnect_dropbox/$', 'disconnect_dropbox', name='disconnect_dropbox'),
-
-    url(r'^upload_picture/$', 'upload_picture', name='upload_picture'),
-    url(r'^save_uploaded_picture/$', 'save_uploaded_picture', name='save_uploaded_picture'),
+urlpatterns = patterns('parsifal.activities.views',
+    url(r'^follow/$', 'follow', name='follow'),
+    url(r'^unfollow/$', 'unfollow', name='unfollow'),
+    url(r'^update_followers_count/$', 'update_followers_count', name='update_followers_count'),
 )
